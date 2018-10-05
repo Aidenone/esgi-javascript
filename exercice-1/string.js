8c8cfd2a8dd161897d0766ba6e1bf151483d62c3
Antoine LUCAS <antoinelucas@MacBook-Pro-de-Antoine.local>
2018-10-05 20:08:24 +0200

    fin exo 1

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -16,4 +16,18 @@ function camelCase(str) {
     str = str.split(' ').join('');
     return str;
 }
-console.log(camelCase(" oui njnejscn hdbfshdf dnbfs     nsdfbdh    "));
+
+function prop_access(object, path) {
+    var array_path = path.split(".");
+    var obj = object;
+    for (var attr in array_path) {
+        var test = array_path[attr];
+        obj = obj[test];
+        if(obj === undefined) return path+" doesn't exist.";
+    }
+    if(!obj) {
+        return object;
+    }
+}
+
+//Object.setPrototypeOf(Correction, ...)
